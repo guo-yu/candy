b733c51b1773b6a7d1144d0e6646e5213e9ec680
guo-yu <o.u.turing@gmail.com>
2013-08-08 03:54:30 +0800

    angular app done

--- public/javascripts/store.js
+++ public/javascripts/store.js
@@ -1,18 +1,9 @@
 // 定义这个数据仓库
 var store = angular.module('store', ['ngResource']).factory('Store', function($resource) {
   return {
-    // define user api
-    user: $resource('/user/:id', {id:'@uid'}),
-    // define article api
-    article : $resource('/article/:id', {id:'@aid'}),
-    // define jsonp api
-    remote : $resource('/remoteData/:id', {id:'@rid'},{
-      jsonp :{
-        method:'JSONP',
-        params:{
-          yourKey: 'yourValue'
-        }
-      }
-    })
+    user: $resource('/user/:action', {action:'@action'}),
+    board: $resource('/board/:action', {action:'@action'}),
+    thread : $resource('/thread/:action', {action:'@action'}),
+    setting : $resource('/setting', {})
   }
 })
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,19 @@
-// index page
-module.exports = function(req, res){
-  res.render('admin/index');
+// admin panel
+var config = require('../ctrlers/config');
+
+exports.page = function(req, res) {
+    res.render('admin/index');
+};
+
+exports.update = function(req, res) {
+    var setting = req.body.setting;
+    if (setting) {
+        config.update(setting._id, setting, function(stat) {
+            res.json(stat);
+        });
+    } else {
+        res.json({
+            stat: 'fail'
+        })
+    }
 };
