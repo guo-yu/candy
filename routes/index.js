4ece6b9513a3521550377340545007eb4333b227
guo-yu <o.u.turing@gmail.com>
2013-08-09 19:33:41 +0800

    更新首页

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 // index page
 var board = require('../ctrlers/board'),
-    async = require('async');
+    async = require('async'),
+    _ = require('underscore');
+
+var wash = function(count,boards) {
+    var threads = [],
+        count = {
+            boards: count.boards ? count.boards: 3,
+            threads: count.threads ? count.threads: 3
+        }
+    _.each(boards,function(b,bindex){
+        if (bindex <= count.boards) {
+            if (b.threads.length > 0) {
+                _.each(b.threads,function(t,tindex){
+                    if (tindex <= count.threads) {
+                        threads.push(t);
+                    }
+                })
+            }
+        }
+    });
+    return threads;
+}
 
 module.exports = function(req, res) {
     async.waterfall([
@@ -10,8 +31,12 @@ module.exports = function(req, res) {
             })
         },
         function(boards, callback) {
+            callback(null, boards, wash(boards));
+        },
+        function(boards,threads, callback) {
             res.render('index', {
-                boards: boards
+                boards: boards,
+                threads: threads
             })
         }
     ]);
