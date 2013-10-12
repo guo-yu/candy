71fb7700324feab8cce15667f47d0b54b21e6135
guo-yu <o.u.turing@gmail.com>
2013-10-13 01:23:35 +0800

    update base ctrler

--- ctrlers/index.js
+++ ctrlers/index.js
@@ -2,75 +2,65 @@ var Ctrler = function(model) {
     this.model = (model) ? model : null;
 }
 
-// create model instance
-Ctrler.prototype.create = function(baby, cb) {
+Ctrler.prototype.create = function(baby, callback) {
     var baby = new this.model(baby);
     baby.save(function(err) {
-        cb(err, baby);
+        callback(err, baby);
     });
 }
 
-// remove by ObjectID
-Ctrler.prototype.remove = function(id, cb) {
-    this.model.findByIdAndRemove(id, function(err) {
-        cb(err, id);
-    });
-}
-
-// update by ObjectID
-Ctrler.prototype.update = function(id, baby, cb) {
-    this.model.findByIdAndUpdate(id, baby, function(err, result) {
-        cb(err, result);
-    });
-}
-
-// read single data by ObjectID
-Ctrler.prototype.read = function(id, cb) {
+Ctrler.prototype.read = function(id, callback) {
     if (id && id.match(/^[0-9a-fA-F]{24}$/)) {
-        this.model.findById(id).exec(function(err, body) {
-            cb(err, body);
-        });
+        this.model.findById(id).exec(callback);
     }
 }
 
-// list all collection
-Ctrler.prototype.list = function(cb) {
-    this.model.find({}).exec(function(err, body) {
-        cb(err, body);
-    });
+Ctrler.prototype.update = function(id, baby, callback) {
+    this.model.findByIdAndUpdate(id, baby, callback);
 }
 
-// list by page
-Ctrler.prototype.page = function(page, limit, params, cb) {
+Ctrler.prototype.remove = function(id, callback) {
+    this.model.findByIdAndRemove(id, callback);
+}
+
+Ctrler.prototype.count = function(params, callback) {
+    var cb = (!callback && typeof(params) === 'function') ? params : callback,
+        query = (params && typeof(params) === 'object') ? params : {};
+    this.model.count(query, cb);
+}
+
+Ctrler.prototype.list = function(params, callback) {
+    var cb = (!callback && typeof(params) === 'function') ? params : callback,
+        query = (params && typeof(params) === 'object') ? params : {};
+    this.model.find(query).exec(cb);
+}
+
+Ctrler.prototype.page = function(page, limit, params, callback) {
     var self = this,
         from = (page && page > 1) ? ((page - 1) * limit) : 0;
     self.model.count(params).exec(function(err, count) {
         if (!err) {
             self.model.find(params).skip(from).limit(limit).exec(function(err, results) {
-                cb(err, results, {
+                callback(err, results, {
                     limit: limit,
                     current: page ? page : 1,
                     max: Math.round(count / limit)
                 });
             });
         } else {
-            cb(err);
+            callback(err);
         }
     });
 }
 
 // advanced find
-Ctrler.prototype.find = function(params, cb) {
-    this.model.find(params).exec(function(err, body) {
-        cb(err, body);
-    });
+Ctrler.prototype.find = function(params, callback) {
+    this.model.find(params).exec(callback);
 }
 
 // advanced findOne
-Ctrler.prototype.findOne = function(params, cb) {
-    this.model.findOne(params).exec(function(err, body) {
-        cb(err, body);
-    });
+Ctrler.prototype.findOne = function(params, callback) {
+    this.model.findOne(params).exec(callback);
 }
 
 // advanced query:
