13bf67db038bc4a22160d59e48d8938aea8d1ecb
Guo Yu <i@guoyu.me>
2013-10-23 15:09:44 +0800

    0.1.7: 根据baseCtrler重构，尚未测试完成

--- ctrlers/config.js
+++ ctrlers/config.js
@@ -1,50 +1,17 @@
-var model = require('../models/index'),
+var Ctrler = require('./index'),
+    model = require('../models/index'),
     config = model.config;
 
-var wash = function(baby) {
-    var baby = baby;
-    if (baby.hasOwnProperty('_id')) {
-        delete baby._id;
-    }
-    if (baby.hasOwnProperty('__v')) {
-        delete baby.__v;
-    }
-    return baby
+var Config = new Ctrler(config);
+
+exports.read = function(callback) {
+    config.findOne({}).exec(callback);
 }
 
-// 写入配置
-exports.create = function(baby,cb){
-    var baby = new config(baby);
-    baby.save(function(err){
-        cb(err, baby);
-    })
-}
-
-// update 
-exports.update = function(id,baby,cb) {
-    var baby = wash(baby);
-    config.findByIdAndUpdate(id,baby,function(err,result){
-        cb(err, result);
-    })
-}
-
-// 读取配置
-exports.readById = function(cb) {
-    config.findById(id).exec(function(err,body){
-        cb(err, body);
+exports.check = function(callback) {
+    this.count(function(err, counts){
+        callback(err, (counts !== 0))
     });
 }
 
-// 读取配置
-exports.read = function(cb) {
-    config.findOne({}).exec(function(err,body){
-        cb(err, body);
-    });
-}
-
-// 检查是否已经写入
-exports.check = function(cb) {
-    config.count().exec(function(err,conuts){
-        cb(err, conuts);
-    })
-}
+module.exports = Config;
--- ctrlers/thread.js
+++ ctrlers/thread.js
@@ -1,65 +1,19 @@
-var model = require('../models/index'),
+var Ctrler = require('./index'),
+	model = require('../models/index'),
 	thread = model.thread,
 	board = require('./board'),
 	user = require('./user'),
 	async = require('async');
 
-// list thread
-exports.ls = function(cb) {
-	thread.find({}).exec(function(err, threads) {
-		cb(err, threads);
-	});
-}
+var Thread = new Ctrler(thread);
 
-// list thread by board id
-exports.lsByBoardId = function(bid, params, cb) {
+Thread.lsByBoardId = function(bid, params, callback) {
 	thread.find({
 		board: bid
-	}).skip(params.from).limit(params.limit).sort('-pubdate').populate('lz').populate('board').exec(function(err, threads) {
-		cb(err, threads);
-	});
+	}).skip(params.from).limit(params.limit).sort('-pubdate').populate('lz').populate('board').exec(callback);
 }
 
-// 查看当前用户是否是楼主 或者 是否是admin用户
-exports.checkLz = function(tid, uid, cb) {
-	thread.findById(tid).populate('media').exec(function(err, thread) {
-		if (!err) {
-			if (thread) {
-				if (thread.lz == uid) {
-					cb(null, true, thread)
-				} else {
-					user.checkAdmin(uid, function(err, result) {
-						if (!err) {
-							if (result) {
-								cb(null, true, thread)
-							} else {
-								cb(null, false)
-							}
-						} else {
-							cb(err)
-						}
-					})
-				}
-			} else {
-				cb(null, false)
-			}
-		} else {
-			cb(err)
-		}
-	});
-}
-
-exports.read = function(id, cb) {
-	if (id && id.match(/^[0-9a-fA-F]{24}$/)) {
-		thread.findById(id).populate('lz').populate('board').populate('media').exec(function(err, thread) {
-			cb(err, thread);
-		});
-	} else {
-		cb(new Error('404'));
-	}
-}
-
-exports.create = function(baby, cb) {
+Thread.create = function(baby, cb) {
 	var baby = new thread(baby);
 	async.waterfall([
 		function(callback) {
@@ -94,15 +48,39 @@ exports.create = function(baby, cb) {
 	], cb);
 }
 
-exports.update = function(id, body, cb) {
-	thread.findByIdAndUpdate(id, body, function(err) {
-		cb(err, body);
-	})
+Thread.read = function(id, callback) {
+	if (this.checkId(id)) {
+		thread.findById(id).populate('lz').populate('board').populate('media').exec(callback);
+	} else {
+		cb(new Error('404'));
+	}
 }
 
-// 删除之后要删除在相应board的索引？
-exports.remove = function(id, cb) {
-	thread.findByIdAndRemove(id, function(err) {
-		cb(err, id);
-	})
+Thread.checkLz = function(tid, uid, cb) {
+	thread.findById(tid).populate('media').exec(function(err, thread) {
+		if (!err) {
+			if (thread) {
+				if (thread.lz == uid) {
+					cb(null, true, thread)
+				} else {
+					user.checkAdmin(uid, function(err, result) {
+						if (!err) {
+							if (result) {
+								cb(null, true, thread)
+							} else {
+								cb(null, false)
+							}
+						} else {
+							cb(err)
+						}
+					})
+				}
+			} else {
+				cb(null, false)
+			}
+		} else {
+			cb(err)
+		}
+	});
 }
+module.exports = Thread;
