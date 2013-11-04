f3fe484c12e9bf555fc569746e3132bef8efb2a4
Guo Yu <i@guoyu.me>
2013-11-04 12:12:16 +0800

    0.1.8: 按照express resource方式修改路由

--- routes/index.js
+++ routes/index.js
@@ -25,32 +25,36 @@ module.exports = function(app) {
     app.get('/signin', App(app), sign.signin);
     app.get('/signout', sign.signout);
 
-    // board
-    app.get('/board/ls', passport, board.ls);
-    app.get('/board/:url', passport, board.read);
-    app.get('/board/:url/page/:page', passport, board.read);
-    app.post('/board/new', check, board.create);
-    app.post('/board/:id', check, board.update);
-    app.delete('/board/:id/remove', check, board.remove);
+    // boards
+    app.resource('boards', board);
+    // app.get('/board/ls', passport, board.ls);
+    // app.get('/board/:url', passport, board.read);
+    // app.get('/board/:url/page/:page', passport, board.read);
+    // app.post('/board/new', check, board.create);
+    // app.post('/board/:id', check, board.update);
+    // app.delete('/board/:id/remove', check, board.remove);
 
-    // thread
-    app.get('/thread/new', check, thread.new);
-    app.post('/thread/new', check, thread.create);
-    app.get('/thread/list', check, thread.ls);
-    app.get('/thread/:id', passport, thread.read);
-    app.get('/thread/:id/edit', check, thread.edit);
-    app.post('/thread/:id/update', check, thread.update);
-    app.delete('/thread/:id/remove', check, thread.remove);
+    // threads
+    app.resource('threads', thread);
+    // app.get('/thread/new', check, thread.new);
+    // app.post('/thread/new', check, thread.create);
+    // app.get('/thread/list', check, thread.ls);
+    // app.get('/thread/:id', passport, thread.read);
+    // app.get('/thread/:id/edit', check, thread.edit);
+    // app.post('/thread/:id/update', check, thread.update);
+    // app.delete('/thread/:id/remove', check, thread.remove);
 
-    // media
-    app.get('/download/:id', passport, media.download);
-    app.post('/upload', check, media.upload);
+    // medias
+    app.resource('medias', thread);
+    // app.get('/download/:id', passport, media.download);
+    // app.post('/upload', check, media.upload);
 
     // user
-    app.get('/user/:id', passport, user.read);
-    app.post('/user/sync', check, App(app), user.sync);
-    app.post('/user/:id', user.update);
-    app.delete('/user/remove', user.remove);
+    app.resource('medias', thread);
+    // app.get('/user/:id', passport, user.read);
+    // app.post('/user/sync', check, App(app), user.sync);
+    // app.post('/user/:id', user.update);
+    // app.delete('/user/remove', user.remove);
 
     // user center
     app.get('/member/:id', passport, user.mime);
