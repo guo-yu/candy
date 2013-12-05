3a115c1c521ba5ba0240a784fcf498c01a2b737a
Guo Yu <i@guoyu.me>
2013-12-05 20:16:41 +0800

    0.1.7: 重构路由

--- routes/home.js
+++ routes/home.js
@@ -1,59 +1,65 @@
 // index page
-var board = require('../ctrlers/board'),
-    thread = require('../ctrlers/thread'),
-    async = require('async');
+var async = require('async');
 
-// borads are id list
-var wash = function(boards, params, callback) {
-    var threads = [];
-    var fetchThreads = function(b, cb) {
-        thread.lsByBoardId(b, {
-            limit: params.limit
-        }, function(err, ts) {
-            if (!err) {
-                if (ts && ts.length > 0) {
-                    threads = threads.concat(ts);
+exports = module.exports = function($ctrlers) {
+
+    var board = $ctrlers.board,
+        thread = $ctrlers.thread;
+
+    // borads are id list
+    // 这里需要清理一下逻辑，太乱了
+    var wash = function(boards, params, callback) {
+        var threads = [];
+        var fetchThreads = function(b, cb) {
+            thread.lsByBoardId(b, {
+                limit: params.limit
+            }, function(err, ts) {
+                if (!err) {
+                    if (ts && ts.length > 0) {
+                        threads = threads.concat(ts);
+                    }
+                    cb();
+                } else {
+                    cb(err)
                 }
-                cb();
-            } else {
-                cb(err)
-            }
-        })
-    };
-    async.each(boards, fetchThreads, function(err) {
-        if (!err) {
-            callback(null, threads);
-        } else {
-            callback(err)
-        }
-    });
-}
-
-// PAGE: 首页
-module.exports = function(req, res, next) {
-    async.waterfall([
-        function(callback) {
-            board.lsId({
-                limit: 10
-            }, function(err, boards) {
-                callback(err, boards);
             })
-        },
-        function(boards, callback) {
-            wash(boards, {
-                limit: 5
-            }, function(err, threads) {
-                callback(err, boards, threads)
-            });
-        }
-    ],function(err, boards, threads){
-        if (!err) {
-            res.render('index', {
-                boards: boards,
-                threads: threads
-            });
-        } else {
-            next(err);
-        }
-    });
-};
+        };
+        async.each(boards, fetchThreads, function(err) {
+            if (!err) {
+                callback(null, threads);
+            } else {
+                callback(err)
+            }
+        });
+    }
+
+    // PAGE: 首页
+    return function(req, res, next) {
+        async.waterfall([
+            function(callback) {
+                board.lsId({
+                    limit: 10
+                }, function(err, boards) {
+                    callback(err, boards);
+                })
+            },
+            function(boards, callback) {
+                wash(boards, {
+                    limit: 5
+                }, function(err, threads) {
+                    callback(err, boards, threads)
+                });
+            }
+        ], function(err, boards, threads) {
+            if (!err) {
+                res.render('index', {
+                    boards: boards,
+                    threads: threads
+                });
+            } else {
+                next(err);
+            }
+        });
+    };
+
+}
--- routes/thread.js
+++ routes/thread.js
@@ -6,9 +6,7 @@
 // PUT     /:id       ->  update
 // DELETE  /:id       ->  destroy
 
-var thread = require('../ctrlers/thread'),
-    board = require('../ctrlers/board'),
-    marked = require('marked'),
+var marked = require('marked'),
     hljs = require('highlight.js');
 
 marked.setOptions({
@@ -18,164 +16,166 @@ marked.setOptions({
     }
 });
 
-// 简单的自增计数
-var visited = function(thread, cb) {
-    thread.views = thread.views + 1;
-    thread.save(function(err) {
-        if (!err) {
-            cb(null)
-        } else {
-            cb(err)
-        }
-    })
-}
+exports = module.exports = function($ctrlers) {
 
-// 列出所有帖子
-exports.index = function(req, res, next) {
-    thread.ls(function(err, ths) {
-        if (!err) {
-            res.json({
-                stat: 'ok',
-                threads: ths
-            })
-        } else {
-            next(err)
-        }
-    })
-}
+    var thread = $ctrlers.thread,
+        board = $ctrlers.board;
 
-// 新增话题页面
-exports.new = function(req, res, next) {
-    // 需要添加识别默认板块的逻辑
-    if (req.query.bid) {
-        board.findById(req.query.bid, function(err, b) {
+    // 简单的自增计数
+    var visited = function(thread, cb) {
+        thread.views = thread.views + 1;
+        thread.save(function(err) {
             if (!err) {
-                res.render('thread/new', {
-                    board: b
-                });
+                cb(null)
             } else {
-                next(err)
-            }
-        })
-    } else {
-        board.readDefault(function(err, b) {
-            if (!err) {
-                res.render('thread/new', {
-                    board: b
-                });
-            } else {
-                next(err)
+                cb(err)
             }
         })
     }
-}
 
-// 查看话题页面
-exports.show = function(req, res, next) {
-    thread.read(req.params.thread, function(err, t) {
-        if (!err) {
-            if (t) {
-                visited(t, function(err) {
-                    // 容忍无法自增浏览数的情况出现，暂时不处理error
-                    res.render('thread/index', {
-                        thread: t,
-                        marked: marked,
-                        error: err
-                    });
-                })
-            } else {
-                next(new Error('404'))
-            }
-        } else {
-            next(err)
-        }
-    });
-}
-
-// 更新帖子页面
-exports.edit = function(req, res, next) {
-    thread.checkLz(req.params.thread, res.locals.user._id, function(err, lz, thread) {
-        if (!err) {
-            if (lz) {
-                res.render('thread/edit', {
-                    thread: thread
-                })
-            } else {
-                next(new Error('404'))
-            }
-        } else {
-            next(err)
-        }
-    })
-}
-
-// API：更新话题
-exports.update = function(req, res, next) {
-    thread.checkLz(req.params.thread, res.locals.user._id, function(err, lz, th) {
-        if (!err) {
-            if (lz) {
-                var updatedThread = {
-                    name: req.body.thread.name,
-                    content: req.body.thread.content,
-                    pubdate: th.pubdate,
-                    views: th.views,
-                    board: th.board,
-                    lz: th.lz
-                };
-                if (req.body.thread.media) updatedThread.media = req.body.thread.media;
-                thread.update(req.params.thread, updatedThread, function(err, thread) {
+    return {
+        // 列出所有帖子
+        index: function(req, res, next) {
+            thread.ls(function(err, ths) {
+                if (!err) {
+                    res.json({
+                        stat: 'ok',
+                        threads: ths
+                    })
+                } else {
+                    next(err)
+                }
+            })
+        },
+        // 新增话题页面
+        new: function(req, res, next) {
+            // 需要添加识别默认板块的逻辑
+            if (req.query.bid) {
+                board.findById(req.query.bid, function(err, b) {
                     if (!err) {
-                        res.json({
-                            stat: 'ok',
+                        res.render('thread/new', {
+                            board: b
+                        });
+                    } else {
+                        next(err)
+                    }
+                })
+            } else {
+                board.readDefault(function(err, b) {
+                    if (!err) {
+                        res.render('thread/new', {
+                            board: b
+                        });
+                    } else {
+                        next(err)
+                    }
+                })
+            }
+        },
+        // API：创建话题
+        create: function(req, res, next) {
+            thread.create(req.body.thread, function(err, baby) {
+                if (!err) {
+                    res.json({
+                        stat: 'ok',
+                        thread: baby
+                    })
+                } else {
+                    next(err)
+                }
+            })
+        },
+        // 查看话题页面
+        show: function(req, res, next) {
+            thread.read(req.params.thread, function(err, t) {
+                if (!err) {
+                    if (t) {
+                        visited(t, function(err) {
+                            // 容忍无法自增浏览数的情况出现，暂时不处理error
+                            res.render('thread/index', {
+                                thread: t,
+                                marked: marked,
+                                error: err
+                            });
+                        })
+                    } else {
+                        next(new Error('404'))
+                    }
+                } else {
+                    next(err)
+                }
+            });
+        },
+        // 更新帖子页面
+        edit: function(req, res, next) {
+            thread.checkLz(req.params.thread, res.locals.user._id, function(err, lz, thread) {
+                if (!err) {
+                    if (lz) {
+                        res.render('thread/edit', {
                             thread: thread
                         })
                     } else {
-                        next(err);
+                        next(new Error('404'))
                     }
-                });
-            } else {
-                next(new Error('not authed'))
-            }
-        } else {
-            next(err)
-        }
-    })
-}
-
-// API：创建话题
-exports.create = function(req, res, next) {
-    thread.create(req.body.thread, function(err, baby) {
-        if (!err) {
-            res.json({
-                stat: 'ok',
-                thread: baby
+                } else {
+                    next(err)
+                }
             })
-        } else {
-            next(err)
-        }
-    })
-}
-
-// API：删除话题
-exports.destroy = function(req, res, next) {
-    thread.checkLz(req.params.thread, res.locals.user._id, function(err, lz, th) {
-        if (!err) {
-            if (lz) {
-                thread.remove(req.params.thread, function(err, tid) {
-                    if (!err) {
-                        res.json({
-                            stat: 'ok',
-                            tid: tid
+        },
+        // API：更新话题
+        update: function(req, res, next) {
+            thread.checkLz(req.params.thread, res.locals.user._id, function(err, lz, th) {
+                if (!err) {
+                    if (lz) {
+                        var updatedThread = {
+                            name: req.body.thread.name,
+                            content: req.body.thread.content,
+                            pubdate: th.pubdate,
+                            views: th.views,
+                            board: th.board,
+                            lz: th.lz
+                        };
+                        if (req.body.thread.media) updatedThread.media = req.body.thread.media;
+                        thread.update(req.params.thread, updatedThread, function(err, thread) {
+                            if (!err) {
+                                res.json({
+                                    stat: 'ok',
+                                    thread: thread
+                                })
+                            } else {
+                                next(err);
+                            }
                         });
                     } else {
-                        next(err);
+                        next(new Error('not authed'))
                     }
-                })
-            } else {
-                next(new Error('not authed'))
-            }
-        } else {
-            next(err)
+                } else {
+                    next(err)
+                }
+            })
+        },
+        // API：删除话题
+        destroy: function(req, res, next) {
+            thread.checkLz(req.params.thread, res.locals.user._id, function(err, lz, th) {
+                if (!err) {
+                    if (lz) {
+                        thread.remove(req.params.thread, function(err, tid) {
+                            if (!err) {
+                                res.json({
+                                    stat: 'ok',
+                                    tid: tid
+                                });
+                            } else {
+                                next(err);
+                            }
+                        })
+                    } else {
+                        next(new Error('not authed'))
+                    }
+                } else {
+                    next(err)
+                }
+            })
         }
-    })
+    }
 }
