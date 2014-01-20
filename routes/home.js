059e3b4a2c338d4cdbcac5c5bb28503b8ee2c121
guo-yu <o.u.turing@gmail.com>
2014-01-20 18:12:38 +0800

    0.1.7: 分离多说登录中间件

--- routes/home.js
+++ routes/home.js
@@ -11,20 +11,6 @@ exports = module.exports = function($ctrlers) {
                     pager: pager
                 });
             });
-        },
-        page: function(req, res, next) {
-            if (!req.params.page) return next(new Error('404'));
-            if (!isNaN(parseInt(req.params.page))) return next(new Error('404'));
-            var page = parseInt(req.params.page, 10);
-            thread.fetchByPage(page, 20, {}, function(err, threads, pager) {
-                if (err) return next(err);
-                if (!threads) return next(new Error('404'));
-                if (threads.length === 0) return next(new Error('404'));
-                res.render('index', {
-                    threads: threads,
-                    pager: pager
-                });
-            });
         }
     }
 
--- routes/page.js
+++ routes/page.js
@@ -1,23 +1,41 @@
 // GET     /page/:page       ->  show
-
 exports = module.exports = function($ctrlers) {
 
-    var board = $ctrlers.board;
+    var board = $ctrlers.board,
+        thread = $ctrlers.thread;
 
     return {
         // PAGE: show selected page
         show: function(req, res, next) {
+            if (!req.params.page) return next(new Error('404'));
+            if (!isNaN(parseInt(req.params.page))) return next(new Error('404'));
+            if (!req.params.thread) return next(new Error('404'));
             if (!req.params.board) return next(new Error('404'));
-            if (!board.checkId(req.params.board)) return next(new Error('404'));
-            board.readByUrl(req.params.board, req.params.page, function(err, b) {
-                if (err) return next(err);
-                if (!b) return next(new Error('404'));
-                res.render('board', {
-                    board: b.board,
-                    threads: b.threads,
-                    page: b.page
+            var page = parseInt(req.params.page);
+            if (req.params.board) {
+                // pager of board
+                if (!board.checkId(req.params.board)) return next(new Error('404'));
+                board.readByUrl(req.params.board, page, function(err, b) {
+                    if (err) return next(err);
+                    if (!b) return next(new Error('404'));
+                    res.render('board', {
+                        board: b.board,
+                        threads: b.threads,
+                        page: b.page
+                    });
                 });
-            });
+            } else {
+                // pager of thread
+                thread.fetchByPage(page, 20, {}, function(err, threads, pager) {
+                    if (err) return next(err);
+                    if (!threads) return next(new Error('404'));
+                    if (threads.length === 0) return next(new Error('404'));
+                    res.render('index', {
+                        threads: threads,
+                        pager: pager
+                    });
+                });
+            }
         }
     }
 
