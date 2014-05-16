95acab1d108b5a9439e15bdadd2d387df7388466
guo-yu <o.u.turing@gmail.com>
2014-05-16 23:48:17 +0800

    0.2.0: 重构路由，以适配 Express 4.x

--- routes/board.js
+++ routes/board.js
@@ -1,16 +1,19 @@
-// GET     /board              ->  index
-// GET     /board/new          ->  new
-// POST    /board              ->  create
-// GET     /board/:board       ->  show
-// GET     /board/:board/edit  ->  edit
-// PUT     /board/:board       ->  update
-// DELETE  /board/:board       ->  destroy
+module.exports = function(deps) {
 
-module.exports = function(ctrlers, theme) {
+  var ctrlers = deps.ctrlers;
+  var express = deps.express;
+  var theme = deps.theme;
+  var locals = deps.locals;
+
+  var Board = express.Router();
   var board = ctrlers.board;
-  return {
+
+  var pagelimit = locals.site.pagelimit || 20;
+
+  // => /board
+  Board.route('/')
     // API: 列出所有板块
-    index: function(req, res, next) {
+    .get(function(req, res, next) {
       board.lsName(function(err, boards) {
         if (err) return next(err);
         return res.json({
@@ -18,9 +21,9 @@ module.exports = function(ctrlers, theme) {
           boards: boards
         });
       })
-    },
+    })
     // API: 创建板块
-    create: function(req, res, next) {
+    .post(function(){
       if (!res.locals.user) return next(new Error('signin required'));
       board.create(res.locals.user._id, req.body.board, function(err, baby) {
         if (err) return next(err);
@@ -29,9 +32,12 @@ module.exports = function(ctrlers, theme) {
           board: baby
         });
       });
-    },
+    });
+
+  // => /board/:board
+  Board.route('/:board')
     // API: 更新板块信息
-    update: function(req, res, next) {
+    .put(function(req, res, next){
       if (!res.locals.user) return next(new Error('signin required'));
       board.update(req.params.board, req.body.board, function(err, board) {
         if (err) return next(err);
@@ -40,9 +46,9 @@ module.exports = function(ctrlers, theme) {
           board: board
         });
       });
-    },
+    })
     // API: 删除板块
-    destroy: function(req, res, next) {
+    .delete(function(req, res, next){
       if (!res.locals.user) return next(new Error('signin required'));
       board.remove(req.params.board, function(err, bid) {
         if (err) return next(err);
@@ -51,6 +57,32 @@ module.exports = function(ctrlers, theme) {
           bid: bid
         });
       })
-    }
-  }
+    });
+
+  // => /board/:board/page/:page
+  Board.get('/:board/page/:page', function(req, res, next){
+    var page = isPage(req.params.page) || 1;
+    // pager of board
+    var query = {}
+    query.url = req.params.board;
+    board.fetch(page, pagelimit, query, function(err, result) {
+      if (err) return next(err);
+      if (!result) return next(new Error('404'));
+      if (result.page.max > 1 && result.threads.length === 0) return next(new Error('404'));
+      theme.render('/board/index', result, function(err, html) {
+        if (err) return next(err);
+        return res.send(html);
+      });
+    });
+  });
+
+  return Board;
+
+}
+
+function isPage(p) {
+  if (!p) return false;
+  var n = parseInt(p);
+  if (isNaN(n)) return false;
+  return n;
 }
--- routes/sign.js
+++ routes/sign.js
@@ -1,59 +1,64 @@
 var async = require('async');
+var Duoshuo = require('duoshuo');
 
-module.exports = function(ctrlers, theme) {
+module.exports = function(deps) {
 
+  var ctrlers = deps.ctrlers;
+  var locals = deps.locals;
+  var express = deps.express;
+
+  var Sign = express.Router();
   var user = ctrlers.user;
+  var duoshuo = new Duoshuo(locals.site.duoshuo);
 
-  return {
-    // PAGE: 登录页面
-    sign: function(req, res, next) {
-      if (res.locals.user) return res.redirect('/');
-      theme.render('/sign', {}, function(err, html) {
-        if (err) return next(err);
-        return res.send(html);
-      });
-    },
-    // PAGE: 登入
-    signin: function(req, res, next) {
-      if (!res.locals.duoshuo) return next(new Error('多说登录失败'));
-      var result = res.locals.duoshuo;
-      // 当返回正确时
-      async.waterfall([
+  // => /sign
+  // PAGE: show sign in page.
+  Sign.get('/', function(req, res, next){
+    if (res.locals.user) return res.redirect('/');
+    theme.render('/sign', {}, function(err, html) {
+      if (err) return next(err);
+      return res.send(html);
+    });
+  });
 
-        function(callback) {
-          user.readByDsId(result.user_id, callback);
-        },
-        function(u, callback) {
-          if (!u) return user.count(callback);
-          req.session.user = u;
-          return res.redirect('back');
-        },
-        function(count, callback) {
-          if (count == 0) result['type'] = 'admin';
-          user.create({
-            type: result.type ? result.type : 'normal',
-            duoshuo: {
-              user_id: result.user_id,
-              access_token: result.access_token
-            }
-          }, function(err, baby) {
-            callback(err, count, baby);
-          });
-        }
-      ], function(err, count, baby) {
-        if (err) return next(err);
-        req.session.user = baby;
-        if (count == 0) return res.redirect('/admin/');
-        res.redirect('/member/' + req.session.user._id);
-      });
-    },
-    // MIDDLEWARE: 检查用户是否管理员用户
-    checkAdmin: function(req, res, next) {
-      if (!res.locals.user) return res.redirect('/');
-      if (res.locals.user.type != 'admin') return res.redirect('/');
-      return next();
-    }
+  // => /sign/in
+  // PAGE: 登入 via duoshuo
+  Sign.get('/in', duoshuo.signin(), function(req, res, next) {
+    if (!res.locals.duoshuo) return next(new Error('多说登录失败'));
+    var result = res.locals.duoshuo;
+    // 当返回正确时
+    async.waterfall([
+      function(callback) {
+        user.readByDsId(result.user_id, callback);
+      },
+      function(u, callback) {
+        if (!u) return user.count(callback);
+        req.session.user = u;
+        return res.redirect('back');
+      },
+      function(count, callback) {
+        if (count == 0) result['type'] = 'admin';
+        user.create({
+          type: result.type ? result.type : 'normal',
+          duoshuo: {
+            user_id: result.user_id,
+            access_token: result.access_token
+          }
+        }, function(err, baby) {
+          callback(err, count, baby);
+        });
+      }
+    ], function(err, count, baby) {
+      if (err) return next(err);
+      req.session.user = baby;
+      if (count == 0) return res.redirect('/admin/');
+      res.redirect('/member/' + req.session.user._id);
+    });
+  });
+  
+  // => /sign/out
+  Sign.get('/out', middlewares.passport.signout);
 
-  }
+  return Sign;
 
 }
