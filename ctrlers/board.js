005343356364ce1dac6341244fcdee1498fc0afc
Guo Yu <i@guoyu.me>
2014-06-09 12:09:09 +0800

    0.2.0: bug fixed

--- ctrlers/board.js
+++ ctrlers/board.js
@@ -54,10 +54,17 @@ module.exports = function(models, Ctrler) {
   }
 
   // Fetch a group of newbie board.
-  Board.newbies = function(date, callback) {
-    var query = {};
-    query.$lte = date || new Date;
-    return board.count(query).exec(callback);
+  Board.newbies = function(callback) {
+    var d = new Date();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var day = d.getDate();
+    return board.count({
+      created: {
+        $lt: new Date(),
+        $gt: new Date(year + ',' + month + ',' + day)
+      }
+    }).exec(callback);
   }
 
   return Board;
