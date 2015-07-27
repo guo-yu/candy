5d05f11885c702bbc654374a809be8df252880ee
guo-yu <o.u.turing@gmail.com>
2015-07-27 12:51:54 +0800

    Add a new function to utils

--- libs/utils.js
+++ libs/utils.js
@@ -1,5 +1,17 @@
 import path from 'path'
 
-export function findPath(basePath=process.cwd(), relativePath) {
+export function findPath(basePath = process.cwd(), relativePath) {
   return path.resolve(basePath, relativePath)
+}
+
+export function isPage(p) {
+  if (!p) 
+    return false
+
+  var n = parseInt(p)
+
+  if (isNaN(n)) 
+    return false
+
+  return n
 }
